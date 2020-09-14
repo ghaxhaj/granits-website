2300dbfd15901eb9cedfb3b26ccdfb4cfb3bab79
Granit <ghaxhaj@fordham.edu>
2020-09-14 16:35:03 -0400

    added form to contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,32 @@
 import React from "react"
 
 class Contact extends React.Component{
+
+    state = {
+        name: "",
+        number: "",
+        email: "",
+        message: ""
+    }
+
+    eventHandler = (event) => {
+       this.setState({[event.target.name]: event.target.value})
+    //    .then(console.log(this.state))
+    }
+
+
     render(){
         return(
-            <div>This is the Contact Component</div>
+            <div>
+                {/* {console.log(this.state)} */}
+                <form action= "matildo:ghaxhaj@fordham.edu" method= "POST" enctype= "multipart/form-data">
+                    Name: <input type="text" name="name" onChange = {this.eventHandler}></input><br></br>
+                    Number: <input type="text" name="number" onChange = {this.eventHandler}></input><br></br>
+                    E-Mail: <input type="text" name="email" onChange = {this.eventHandler}></input><br></br>
+                    Message: <input type="textArea" name="message" onChange = {this.eventHandler}></input><br></br>
+                    <input type="submit" name="submit" value="Submit"></input>
+                    </form>
+            </div>
         )
     }
 }
