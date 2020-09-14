688375e9b758b3baea4e658d3dee9890cdb3c1a4
Granit <ghaxhaj@fordham.edu>
2020-09-14 11:06:14 -0400

    blog component complete

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,10 +1,40 @@
 import React from "react"
+import "../styles.css"
+import Medium from "../images/medium.png"
 
 class Blogs extends React.Component{
+
+    state = {
+        blogs: []
+    }
+
+   componentDidMount(){
+        fetch('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@ghaxhaj')
+        .then(resp => resp.json())
+        .then(blogs => this.setState({blogs}))
+   }
+
+   renderBlogs = () => {
+    
+    if(this.state.blogs.items){
+    return this.state.blogs.items.map(post => {
+    
+   return <div className="column">
+        <div className="card">
+            {post.thumbnail ? <img src = {post.thumbnail} className = "projectImg"/> : <img src = {Medium} className = "projectImg"/>}
+            <h1 className = "cardHeader">{post.title}</h1>
+    <p className = "cardText">Posted on: {post.pubDate}</p>
+            <a href = {post.link} className = "projectLink">Read the Full Blog Here!</a>
+        </div>
+    </div>})
+    }
+   }
+
     render(){
         return(
-            <div>
-                This is the Blog Component
+            <div className="row">
+                {console.log(this.state.blogs.items)}
+                {this.renderBlogs()}
             </div>
         )
     }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,43 +9,43 @@ class Projects extends React.Component {
     render(){
         return(
             <div>
-                <div class="row">
-                <div class="column">
-                        <div class="card">
+                <div className="row">
+                <div className="column">
+                        <div className="card">
                             <img src = {nbaRef} className = "projectImg"/>
                             <h1 className = "cardHeader">NBA Reference</h1>
                             <p className = "cardText"> NBA Reference is an app that tracks all data from the 2019-20 NBA Season and allows for game simulations. </p>
-                            <a href = "https://github.com/ghaxhaj/NBARef" className = "cardText">Front-End Repository</a><br></br>
-                            <a href = "https://github.com/ghaxhaj/NBARefBackEnd" className = "cardText">Back-End Repository</a><br></br>
-                            <a href = "https://vimeo.com/421127797" className = "cardText">Demo</a>
+                            <a href = "https://github.com/ghaxhaj/NBARef" className = "projectLink">Front-End Repository</a><br></br>
+                            <a href = "https://github.com/ghaxhaj/NBARefBackEnd" className = "projectLink">Back-End Repository</a><br></br>
+                            <a href = "https://vimeo.com/421127797" className = "projectLink">Demo</a>
                         </div>
                     </div>
-                    <div class="column">
-                        <div class="card">
+                    <div className="column">
+                        <div className="card">
                             <img src = {FlatGram} className = "projectImg"/>
                             <h1 className = "cardHeader">FlatGram</h1>
                             <p className = "cardText">FlatGram is a social media platform designed to keep Flatiron graduates connected on a social level. It possesses full CRUD capability.</p>
-                            <a href = "https://github.com/ghaxhaj/flat-gram" className = "cardText">Front-End Repository</a><br></br>
-                            <a href = "https://github.com/sekoudosso82/flat-gram-back-end" className = "cardText">Back-End Repository</a><br></br>
-                            <a href = "https://vimeo.com/421127166" className = "cardText">Demo</a>
+                            <a href = "https://github.com/ghaxhaj/flat-gram" className = "projectLink">Front-End Repository</a><br></br>
+                            <a href = "https://github.com/sekoudosso82/flat-gram-back-end" className = "projectLink">Back-End Repository</a><br></br>
+                            <a href = "https://vimeo.com/421127166" className = "projectLink">Demo</a>
                         </div>
                     </div>
-                    <div class="column">
-                        <div class="card">
+                    <div className="column">
+                        <div className="card">
                             <img src = {aMazeing} className = "projectImg"/>
                             <h1 className = "cardHeader">aMazeing</h1>
                             <p className = "cardText">aMazeing is a web game that auto-generates a maze and gives the user 35 seconds to solve it. It was created with the p5.js library.</p>
-                            <a href = "https://github.com/ftodoroski/aMazeing/tree/Granit" className = "cardText">Front-End Repository</a><br></br>
-                            <a href = "https://vimeo.com/421125815" className = "cardText">Demo</a>
+                            <a href = "https://github.com/ftodoroski/aMazeing/tree/Granit" className = "projectLink">Front-End Repository</a><br></br>
+                            <a href = "https://vimeo.com/421125815" className = "projectLink">Demo</a>
                             <h1></h1>
                         </div>
                     </div>
-                    <div class="column">
-                        <div class="card">
+                    <div className="column">
+                        <div className="card">
                             <img src = {SoundLoud} className = "projectImg"/>
                             <h1 className = "cardHeader">SoundLoud</h1>
                             <p className = "cardText">SoundLoud is a music playlist creation website that allows users to save music videos to their profiles, and allows artists to post videos. It was created with Ruby and Ruby on Rails. </p>
-                            <a href = "https://github.com/ghaxhaj/SoundLoud" className = "cardText">Repository</a>
+                            <a href = "https://github.com/ghaxhaj/SoundLoud" className = "projectLink">Repository</a>
                             <h1></h1>
                         </div>
                     </div>
