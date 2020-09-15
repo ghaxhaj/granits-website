1a3ec7d219dc5715940f46947df1b3bbf34f54cd
Granit <ghaxhaj@fordham.edu>
2020-09-15 12:39:25 -0400

    added email server + fix node/npm packages

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,6 @@
 import React from "react"
+import { init } from "emailjs"
+
 
 class Contact extends React.Component{
 
@@ -14,17 +16,30 @@ class Contact extends React.Component{
     //    .then(console.log(this.state))
     }
 
+    handleSubmit = (event) => {
+      emailjs.send('ghaxhaj@fordham.edu', 'template_t3473fj', this.state)
+      .then(console.log("Your Email has been Sent"))
+    }
 
     render(){
         return(
             <div>
                 {/* {console.log(this.state)} */}
-                <form action= "matildo:ghaxhaj@fordham.edu" method= "POST" enctype= "multipart/form-data">
+                <script type="text/javascript"
+                    src="https://cdn.jsdelivr.net/npm/emailjs-com@2/dist/email.min.js">
+                </script>
+                <script type="text/javascript">
+                {function(){
+                emailjs.init("user_IL9lr2nt8XPG23hR5A284");
+                }}
+                </script>
+
+                <form>
                     Name: <input type="text" name="name" onChange = {this.eventHandler}></input><br></br>
                     Number: <input type="text" name="number" onChange = {this.eventHandler}></input><br></br>
-                    E-Mail: <input type="text" name="email" onChange = {this.eventHandler}></input><br></br>
-                    Message: <input type="textArea" name="message" onChange = {this.eventHandler}></input><br></br>
-                    <input type="submit" name="submit" value="Submit"></input>
+                    E-Mail: <input type="email" name="email" onChange = {this.eventHandler}></input><br></br>
+                    Message: <textarea type="text" name="message" onChange = {this.eventHandler}></textarea><br></br>
+                    <button type = "submit" onClick = {this.handleSubmit}>Submit</button>
                     </form>
             </div>
         )
